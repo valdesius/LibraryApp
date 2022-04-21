f3254a657e0f8161af24742f514f7d36ffc00748
valdesius <v.inc@inbox.ru>
2022-04-21 23:42:45 +0300

    V1. Creating project

---
+++ src/main/java/com/valdesius/App.java
@@ -0,0 +1,12 @@
+package com.valdesius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+    }
+}
