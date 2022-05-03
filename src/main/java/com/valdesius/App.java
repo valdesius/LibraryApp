7afac8720315e0b157bec600684e89d34fd1011c
valdesius <v.inc@inbox.ru>
2022-05-03 21:31:31 +0300

    V2. H2 bd

--- src/main/java/com/valdesius/App.java
+++ src/main/java/com/valdesius/App.java
@@ -1,12 +1,20 @@
 package com.valdesius;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+        try {
+            Console.main(args);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
